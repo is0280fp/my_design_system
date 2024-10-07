58eb248847a237b44638c547d9423e2034fcd82b
is0280fp <umeyume45@gmail.com>
2024-10-07 10:44:18 +0900

    add bgcolor

--- stories/Button/Button.tsx
+++ stories/Button/Button.tsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useMemo } from "react";
 import "./button.css";
 import { ButtonRoleHTMLTag, ButtonType } from "../util";
+import { colors } from "../colors";
 
 interface ButtonProps {
-  // /** Is this the principal call to action on the page? */
-  // primary?: boolean;
-  // /** What background color to use */
-  // backgroundColor?: string;
-  // /** How large should the button be? */
-  // size?: "small" | "medium" | "large";
-  // /** Button contents */
-  // label: string;
-  // /** Optional click handler */
-  // onClick?: () => void;
   as?: ButtonRoleHTMLTag;
   type?: ButtonType;
   href?: string;
   openNewTab?: boolean;
   disabled?: boolean;
+  variant?: "primary" | "secondary" | "tertialy";
+  size?: "default" | "medium" | "small" | "tiny" | "large";
+  color?: string;
+  hoverColor?: string;
+  borderColor?: string;
+  textColor?: string;
+}
+
+type DisabledVariantProps = Pick<ButtonProps, "disabled" | "variant">;
+
+function getBgColor(props: DisabledVariantProps): string {
+  const { disabled, variant } = props;
+
+  if (disabled && variant === "tertialy") {
+    return colors.TextAndIconsColor.Default.OnsurfaceTertiary;
+  } else if (disabled && variant !== "tertialy") {
+    return colors.SurfaceColor.Disabled.SurfaceDisabled;
+  } else if (!disabled && variant === "primary") {
+    return colors.SurfaceColor.Inverse.OnsurfaceInversePrimary;
+  } else if (!disabled && variant === "secondary") {
+    return colors.TextAndIconsColor.Default.OnsurfacePrimary;
+  } else if (!disabled && variant === "tertialy") {
+    return "transparent";
+  }
+  return "transparent"; // Default case, or handle more variants here if needed
 }
 
 /** Primary UI component for user interaction */
@@ -25,6 +41,11 @@ export const Button: React.FC<ButtonProps> = ({
   // define default value
   ...props
 }) => {
+  const bgColor = useMemo(
+    () => getBgColor({ disabled: props.disabled, variant: props.variant }),
+    // what to feed for this?
+    []
+  );
   switch (props.as) {
     case "a":
       return (
