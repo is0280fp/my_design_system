55ea9991ffbdd1d4d0301a21c7afc0459892b356
is0280fp <umeyume45@gmail.com>
2024-10-08 10:11:04 +0900

    add style

--- stories/Button/Button.tsx
+++ stories/Button/Button.tsx
@@ -11,10 +11,13 @@ interface ButtonProps {
   disabled?: boolean;
   variant?: "primary" | "secondary" | "tertialy";
   size?: "default" | "medium" | "small" | "tiny" | "large";
+  width?: number | string;
+  height?: number | string;
   color?: string;
   hoverColor?: string;
   borderColor?: string;
   textColor?: string;
+  className?: string;
 }
 
 type DisabledVariantProps = Pick<ButtonProps, "disabled" | "variant">;
@@ -30,56 +33,88 @@ function getBgColor(props: DisabledVariantProps): string {
     return colors.SurfaceColor.Inverse.OnsurfaceInversePrimary;
   } else if (!disabled && variant === "secondary") {
     return colors.TextAndIconsColor.Default.OnsurfacePrimary;
-  } else if (!disabled && variant === "tertialy") {
+  } else {
     return "transparent";
   }
-  return "transparent"; // Default case, or handle more variants here if needed
 }
 
-/** Primary UI component for user interaction */
+function getTextColor(props: DisabledVariantProps): string {
+  const { disabled, variant } = props;
+  if (disabled) {
+    return colors.TextAndIconsColor.Default.OnsurfaceTertiary;
+  } else if (variant === "primary") {
+    return colors.TextAndIconsColor.Inverse.OnsurfaceInversePrimary;
+  } else {
+    return colors.TextAndIconsColor.Default.OnsurfacePrimary;
+  }
+}
+
 export const Button: React.FC<ButtonProps> = ({
   // define default value
+  as = "button",
+  disabled = false,
+  variant = "primary",
+  size = "default",
   ...props
 }) => {
+  const { className, ...restProps } = props;
+
   const bgColor = useMemo(
-    () => getBgColor({ disabled: props.disabled, variant: props.variant }),
-    // what to feed for this?
-    []
+    () => getBgColor({ disabled: disabled, variant: variant }),
+    [disabled, variant]
   );
-  switch (props.as) {
+  const textColor = useMemo(
+    () => getTextColor({ disabled: disabled, variant: variant }),
+    [disabled, variant]
+  );
+  const buttonClassName = useMemo(
+    () => ["button", className].filter(Boolean).join(" "),
+    [className]
+  );
+  const width = useMemo(() => {
+    if (typeof props?.width === "number") return `${props.width * 4}px`;
+    return props?.width ?? "100%";
+  }, [props.width]);
+
+  const height = useMemo(() => {
+    if (typeof props?.height === "number") return `${props.height * 4}px`;
+    return props?.height ?? "fit-content";
+  }, [props.height]);
+
+  const btnStyle = (bgColor: string, width, height) => ({
+    background: bgColor,
+    width: width,
+    height: height,
+  });
+
+  const textStyle = (textColor) => ({
+    color: textColor,
+  });
+
+  switch (as) {
     case "a":
       return (
         <a
-          // type="button"
-          // className={[
-          //   "storybook-button",
-          //   `storybook-button--${size}`,
-          //   mode,
-          // ].join(" ")}
-          // style={{ backgroundColor }}
           href={props.href}
           target={props.openNewTab ? "_blank" : ""}
           rel={props.openNewTab ? "noopener noreferrer" : ""}
           // {...props}
         >
           {/* {label} */}
+          anker
         </a>
       );
       break;
     case "button":
       return (
         <button
-          // type={props.type}
-          // className={[
-          //   "storybook-button",
-          //   `storybook-button--${size}`,
-          //   mode,
-          // ].join(" ")}
-          // style={{ backgroundColor }}
-          // {...props}
-          disabled={props.disabled}
+          {...restProps}
+          className={buttonClassName}
+          disabled={disabled}
+          style={btnStyle(bgColor, width, height)}
         >
           {/* {label} */}
+          <span style={textStyle(textColor)}>Button</span>
         </button>
       );
       break;
