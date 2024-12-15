3917ec24859c66f582cee65faaf7ae9b68167a60
is0280fp <umeyume45@gmail.com>
2024-12-15 14:00:27 +0900

    fix minor error in button

--- stories/Button/Button.tsx
+++ stories/Button/Button.tsx
@@ -2,6 +2,7 @@ import React, { useMemo } from "react";
 import "./button.css";
 import { ButtonRoleHTMLTag, ButtonType } from "../util";
 import { colors } from "../colors";
+import styled from "styled-components";
 
 interface ButtonProps {
   as?: ButtonRoleHTMLTag;
@@ -18,12 +19,30 @@ interface ButtonProps {
   borderColor?: string;
   textColor?: string;
   className?: string;
+  shape?: "pill" | "rect";
+}
+
+interface StyledButtonProps extends ButtonProps {
+  color: string;
+  hoverColor: string;
+  borderColor: string;
+  borderRadius: string;
+  width?: number | string;
+  height?: number | string;
+  disabled: boolean;
 }
 
 type DisabledVariantProps = Pick<ButtonProps, "disabled" | "variant">;
+type BorderProps = Pick<
+  ButtonProps,
+  "disabled" | "variant" | "borderColor" | "color"
+>;
+type BorderRadiusProps = Pick<ButtonProps, "size" | "shape">;
 
 function getBgColor(props: DisabledVariantProps): string {
   const { disabled, variant } = props;
+  // console.log(disabled);
+  // console.log(variant);
 
   if (disabled && variant === "tertialy") {
     return colors.TextAndIconsColor.Default.OnsurfaceTertiary;
@@ -38,6 +57,36 @@ function getBgColor(props: DisabledVariantProps): string {
   }
 }
 
+function getBorderColor(props: BorderProps): string {
+  const { disabled, variant, borderColor, color } = props;
+  // console.log(disabled);
+  // console.log(variant);
+  // console.log(borderColor);
+  // console.log(color);
+  if (disabled) {
+    return colors.SurfaceColor.Disabled.SurfaceDisabled;
+  } else if (!disabled && borderColor) {
+    return borderColor;
+  } else if (!disabled && !borderColor && color) {
+    return color;
+  } else {
+    return variant === "primary" || variant === "secondary"
+      ? colors.StaticColor.StaticBlack
+      : "transparent";
+  }
+}
+
+function getBorderRadius(props: BorderRadiusProps): string {
+  const { size, shape } = props;
+  console.log(size);
+  console.log(shape);
+  if (shape === "rect") {
+    return ["small", "tiny"].includes(size ?? "default") ? "4px" : "8px";
+  } else {
+    return "30px";
+  }
+}
+
 function getTextColor(props: DisabledVariantProps): string {
   const { disabled, variant } = props;
   if (disabled) {
@@ -48,6 +97,32 @@ function getTextColor(props: DisabledVariantProps): string {
     return colors.TextAndIconsColor.Default.OnsurfacePrimary;
   }
 }
+
+// Styled components for button and text
+const StyledButton = styled.button<StyledButtonProps>`
+  background: ${(props) => props.color};
+  width: ${(props) =>
+    typeof props.width === "number"
+      ? `${props.width * 4}px`
+      : props.width || "100%"};
+  height: ${(props) =>
+    typeof props.height === "number"
+      ? `${props.height * 4}px`
+      : props.height || "fit-content"};
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+  padding: 8px 16px;
+  border: ${(props) => props.borderColor};
+  border-radius: ${(props) => props.borderRadius};
+
+  &:hover {
+    background: ${(props) => props.hoverColor};
+    box-shadow: inset 999px 999px 0px rgba(255, 255, 255, 0.2);
+  }
+`;
+
+const StyledText = styled.span<ButtonProps>`
+  color: ${(props) => getTextColor(props)};
+`;
 
 export const Button: React.FC<ButtonProps> = ({
   // define default value
@@ -60,36 +135,38 @@ export const Button: React.FC<ButtonProps> = ({
   const { className, ...restProps } = props;
 
   const bgColor = useMemo(
-    () => getBgColor({ disabled: disabled, variant: variant }),
+    () => getBgColor({ disabled, variant }),
     [disabled, variant]
   );
-  const textColor = useMemo(
-    () => getTextColor({ disabled: disabled, variant: variant }),
-    [disabled, variant]
-  );
+
+  const hoverColor = useMemo(() => {
+    if (disabled) {
+      return bgColor;
+    } else {
+      return props.hoverColor || bgColor || "transparent";
+    }
+  }, [disabled, props.hoverColor]);
+
+  const borderColor = useMemo(() => {
+    return getBorderColor({
+      disabled,
+      variant,
+      borderColor: restProps.borderColor,
+      color: restProps.color,
+    });
+  }, [disabled, variant, restProps.borderColor, restProps.color]);
+
+  const borderRadius = useMemo(() => {
+    return getBorderRadius({
+      size,
+      shape: restProps.shape,
+    });
+  }, [size, restProps.shape]);
+
   const buttonClassName = useMemo(
     () => ["button", className].filter(Boolean).join(" "),
     [className]
   );
-  const width = useMemo(() => {
-    if (typeof props?.width === "number") return `${props.width * 4}px`;
-    return props?.width ?? "100%";
-  }, [props.width]);
-
-  const height = useMemo(() => {
-    if (typeof props?.height === "number") return `${props.height * 4}px`;
-    return props?.height ?? "fit-content";
-  }, [props.height]);
-
-  const btnStyle = (bgColor: string, width, height) => ({
-    background: bgColor,
-    width: width,
-    height: height,
-  });
-
-  const textStyle = (textColor) => ({
-    color: textColor,
-  });
 
   switch (as) {
     case "a":
@@ -98,24 +175,27 @@ export const Button: React.FC<ButtonProps> = ({
           href={props.href}
           target={props.openNewTab ? "_blank" : ""}
           rel={props.openNewTab ? "noopener noreferrer" : ""}
-          // {...props}
         >
-          {/* {label} */}
           anker
         </a>
       );
-      break;
     case "button":
       return (
-        <button
+        <StyledButton
           {...restProps}
           className={buttonClassName}
           disabled={disabled}
-          style={btnStyle(bgColor, width, height)}
+          color={bgColor}
+          hoverColor={hoverColor}
+          borderColor={borderColor}
+          borderRadius={borderRadius}
+          width={props.width}
+          height={props.height}
         >
-          {/* {label} */}
-          <span style={textStyle(textColor)}>Button</span>
-        </button>
+          <StyledText disabled={disabled} variant={variant}>
+            Button
+          </StyledText>
+        </StyledButton>
       );
       break;
   }
